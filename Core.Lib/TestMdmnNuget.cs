1a1a36158412637bc4da18895ec669c8861f8abc
MaksMdmn <maks.mdmn@icloud.com>
2020-01-25 14:41:25 +0200

    First Commit

---
+++ Core.Lib/TestMdmnNuget.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Core.Lib
+{
+    public class TestMdmnNuget
+    {
+        public static void MethodTest()
+        {
+            Console.WriteLine("hi from nuget!");
+        }
+    }
+}
